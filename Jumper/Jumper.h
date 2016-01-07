005a150991d61a5bc23c210699c9255e492aebe3
Adam Kirk <atomkirk@gmail.com>
2016-01-06 18:32:24 -0700

    init

---
+++ Jumper/Jumper.h
@@ -0,0 +1,19 @@
+//
+//  Jumper.h
+//  Jumper
+//
+//  Created by Adam Kirk on 1/6/16.
+//  Copyright © 2016 Adam Kirk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jumper.
+FOUNDATION_EXPORT double JumperVersionNumber;
+
+//! Project version string for Jumper.
+FOUNDATION_EXPORT const unsigned char JumperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jumper/PublicHeader.h>
+
+
